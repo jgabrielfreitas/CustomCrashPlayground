896b167418bcd25bb6ddaad78ab2606441be0007
JGabrielFreitas <jgabrielafreitas@gmail.com>
2016-12-28 18:54:47 -0200

    In love with Kotlin

---
+++ app/src/main/java/com/jgabrielfreitas/crash/CustomCrashActivity.kt
@@ -0,0 +1,12 @@
+package com.jgabrielfreitas.crash
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class CustomCrashActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_custom_crash)
+  }
+}
--- app/src/main/java/com/jgabrielfreitas/crash/MainActivity.kt
+++ app/src/main/java/com/jgabrielfreitas/crash/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.jgabrielfreitas.crash
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import cat.ereza.customactivityoncrash.CustomActivityOnCrash
+import kotlinx.android.synthetic.main.activity_main.crashButton
 
 class MainActivity : AppCompatActivity() {
+
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
 
     //Install CustomActivityOnCrash
-    CustomActivityOnCrash.install(this);
+    CustomActivityOnCrash.install(this)
+    CustomActivityOnCrash.setShowErrorDetails(false)
+    CustomActivityOnCrash.setRestartActivityClass(CustomCrashActivity::class.java)
 
+    crashButton.setOnClickListener { throw RuntimeException("FUUUUUCK") }
   }
 }
